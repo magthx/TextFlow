c0a7b62bbbea47c9f63ee9a6ccee74ee38fedeb4
dylanmagallon1 <dmagallon2@ucol.mx>
2024-09-27 15:03:39 -0600

    TextFlow project 27/09/2024

---
+++ public/main_window_scripting.js
@@ -0,0 +1,204 @@
+// Importar las funciones necesarias de Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
+
+// Configuración de Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyAu-Et1TK0t-i04xaMgpmDDGDN6aarm7DI",
+    authDomain: "textflow-7d357.firebaseapp.com",
+    projectId: "textflow-7d357",
+    appId: "1:652709206886:web:d054e1d2b2ff381e2dc6a7"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const thisDatabase = getDatabase(app);
+const firestore = getFirestore(app);
+const injection_gray_circle_chat = document.getElementById('injection_gray_circle_chat')
+
+// Manejo del estado de autenticación
+onAuthStateChanged(auth, async (user) => { // Marcamos la función como `async`
+    if (user) {
+        inyeccion_email.textContent = `${user.email}`;
+        injection_gray_circle_chat.className = 'fas fa-circle';
+        try {
+            // Obtener los datos del usuario desde Firestore
+            const userDoc = await getDoc(doc(firestore, 'users', user.uid));
+            if (userDoc.exists()) {
+                const userData = userDoc.data();
+                inyeccion_user.textContent = `${userData.username}`;
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    html: `Documento del usuario no encontrado. Serás redirigido`,
+                    didClose: () => {
+                        window.location.href = '../src/pages/register_on.html';
+                    }
+                });
+            }
+        } catch (error) {
+            Swal.fire({
+                icon: 'error',
+                html: `${error}`,
+                didClose: () => {
+                    window.location.href = '../src/pages/register_on.html';
+                }
+            });
+        }
+    } else {
+        Swal.fire({
+            icon: 'error',
+            html: `No tienes una cuenta, porfavor crea una.`,
+            didClose: () => {
+                window.location.href = '../src/pages/register_on.html';
+            }
+        });
+    }
+});
+
+// VENTANA EN GENERAL //
+const logo_account = document.getElementById('logo-account');
+const ul_user = document.getElementById('ul-user');
+const injection_span_main_interface = document.getElementById('injection-span-main-interface');
+injection_span_main_interface.textContent = `Sala general`;
+logo_account.addEventListener('click', () => {
+    if (ul_user.style.display === 'none' || ul_user.style.display === '') {
+        ul_user.style.display = 'flex';
+    } else {
+        ul_user.style.display = 'none';
+    }
+});
+
+// FUNCIONALIDADES PRINCIPALES DE LA VENTANA //
+const send_user_form = document.getElementById('send--user--form');
+send_user_form.addEventListener('click', (event) => {
+    event.preventDefault();
+    let text_input_from_user = document.getElementById('text--input-from-user').value;
+    text_input_from_user = text_input_from_user.replace(/\s+/g, ' ');
+    text_input_from_user = text_input_from_user.trim();
+    text_input_from_user = text_input_from_user.toLowerCase();
+    if (text_input_from_user < 1) {
+        return;
+    } else {
+
+        // EMPIEZA LO NUEVO //
+
+        onAuthStateChanged(auth, async (user) => { // Marcamos la función como `async`
+            if (user) {
+                inyeccion_email.textContent = `${user.email}`;
+                try {
+                    // Obtener los datos del usuario desde Firestore
+                    const userDoc = await getDoc(doc(firestore, 'users', user.uid));
+                    if (userDoc.exists()) {
+                        const userData = userDoc.data();
+                        inyeccion_user.textContent = `${userData.username}`;
+
+                        const thisUserData = ref(thisDatabase, 'textFlow');
+                        push(thisUserData, {
+                            message: `${text_input_from_user}`,
+                            usuario: `${userData.username}`
+                        }).then(() => {
+                            // accion de subida a firebase exitosa //
+                        }).catch(() => {
+                            Swal.fire(({
+                                icon: 'error',
+                                html: `${error.message}`
+                            }));
+                        });
+
+                        function activarFormulario() {
+                            let text_input_from_user = document.getElementById('text--input-from-user');
+                            text_input_from_user.disabled = false;
+                            text_input_from_user.value = '';
+                            text_input_from_user.placeholder = 'Escribe tu mensaje..';
+                            text_input_from_user.focus();
+                        }
+
+                        function desactivarFormulario() {
+                            let text_input_from_user = document.getElementById('text--input-from-user');
+                            text_input_from_user.disabled = true;
+                            text_input_from_user.value = '';
+                            text_input_from_user.placeholder = 'Espere 3 segundos..';
+                        } desactivarFormulario();
+
+                        setTimeout(() => {
+                            activarFormulario();
+                        }, 3000)
+
+                    } else {
+                        Swal.fire({
+                            icon: 'error',
+                            html: `Documento del usuario no encontrado. Serás redirigido`,
+                            didClose: () => {
+                                window.location.href = '../src/pages/register_on.html';
+                            }
+                        });
+                    }
+                } catch (error) {
+                    Swal.fire({
+                        icon: 'error',
+                        html: `${error}`,
+                        didClose: () => {
+                            window.location.href = '../src/pages/register_on.html';
+                        }
+                    });
+                }
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    html: `No tienes una cuenta, porfavor crea una.`,
+                    didClose: () => {
+                        window.location.href = '../src/pages/register_on.html';
+                    }
+                });
+            }
+        });
+    }
+});
+window.onload = function () {
+    //father
+    let obtenerPadreAllMessage = document.getElementById('user-main-app-window');
+
+    const lecturaMensajes = () => {
+        const referenciaLectura = ref(thisDatabase, 'textFlow');
+
+        onValue(referenciaLectura, (snapshot) => {
+            const obtenerRecuadroMensajes = document.getElementById('user-interface-main-screen');
+            function moveToMessage() {
+                obtenerRecuadroMensajes.scrollTo({
+                    top: obtenerRecuadroMensajes.scrollHeight,
+                    behavior: 'smooth'
+                });
+            }
+            setTimeout(() => {
+                moveToMessage();
+            }, 300)
+            const datos = snapshot.val();
+            obtenerPadreAllMessage.innerHTML = '';
+
+            Object.values(datos).forEach(item => {
+                const newDivMessage = document.createElement('div');
+                const ul = document.createElement('ul');
+                const li = document.createElement('li');
+                const strong = document.createElement('strong');
+                const span = document.createElement('span');
+
+                newDivMessage.classList.add('this--message-newDivMessage')
+
+                strong.textContent = `${item.usuario}:`
+                span.textContent = item.message;
+
+                li.appendChild(strong);
+                li.appendChild(span);
+                ul.appendChild(li)
+                newDivMessage.appendChild(ul);
+                obtenerPadreAllMessage.appendChild(newDivMessage);
+            });
+        });
+
+    }
+    lecturaMensajes();
+}
---
+++ src/scripts/myDataRegister.js
@@ -0,0 +1,216 @@
+// Importar las funciones necesarias de Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import { getFirestore, setDoc, doc, getDoc, query, where, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
+
+// Configuración de Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyAu-Et1TK0t-i04xaMgpmDDGDN6aarm7DI",
+    authDomain: "textflow-7d357.firebaseapp.com",
+    projectId: "textflow-7d357",
+    storageBucket: "textflow-7d357.appspot.com",
+    messagingSenderId: "652709206886",
+    appId: "1:652709206886:web:d054e1d2b2ff381e2dc6a7"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+
+// Registro de usuario
+const thisRegisterForm = document.getElementById('thisRegisterForm');
+thisRegisterForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+
+    let textoCorreo = document.getElementById('textoCorreo').value.toLowerCase();
+    let textoCifrado = document.getElementById('textoCifrado').value;
+    let usuarioPublico = document.getElementById('usuarioPublico').value.toLowerCase();
+
+    // VARIABLES DE DATOS (ELEMENTOS A RESETEAR REGISTER)
+    let textoCorreoElement = document.getElementById('textoCorreo');
+    let textoCifradoElement = document.getElementById('textoCifrado');
+    let usuarioPublicoElement = document.getElementById('usuarioPublico');
+
+    try {
+        // Verificar si el nombre de usuario ya existe
+        const userQuery = query(collection(db, 'users'), where('username', '==', usuarioPublico));
+        const querySnapshot = await getDocs(userQuery);
+
+        if (!querySnapshot.empty) {
+            // Si el nombre de usuario ya existe
+            Swal.fire({
+                icon: 'error',
+                title: 'Nombre de Usuario Existente',
+                text: 'El nombre de usuario ya está en uso. Por favor, elige otro.'
+            });
+            return;
+        }
+
+        // Crear usuario con correo y contraseña
+        const userCredential = await createUserWithEmailAndPassword(auth, textoCorreo, textoCifrado);
+        const user = userCredential.user;
+
+        // Guardar el nombre de usuario y el correo electrónico en Firestore
+        await setDoc(doc(db, 'users', user.uid), {
+            email: textoCorreo,
+            username: usuarioPublico
+        });
+
+        // Mostrar éxito
+        Swal.fire({
+            icon: 'success',
+            html: `<span>Bienvenido <strong>${usuarioPublico}</strong></span>`,
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didClose: () => {
+                window.location.href = '../../public/';
+            }
+        });
+    } catch (error) {
+        textoCorreoElement.value = '';
+        textoCifradoElement.value = '';
+        usuarioPublicoElement.value = '';
+
+        // Deshabilitar campos y mostrar mensaje
+        function deshabilitarCamposElementosRegistro() {
+            textoCorreoElement.disabled = true;
+            textoCorreoElement.placeholder = 'Espere 3 segundos..';
+
+            textoCifradoElement.disabled = true;
+            textoCifradoElement.placeholder = 'Espere 3 segundos..';
+
+            usuarioPublicoElement.disabled = true;
+            usuarioPublicoElement.placeholder = 'Espere 3 segundos..';
+        }
+        deshabilitarCamposElementosRegistro();
+
+        setTimeout(() => {
+            textoCorreoElement.disabled = false;
+            textoCorreoElement.placeholder = 'Ingresa tu correo';
+
+            textoCifradoElement.disabled = false;
+            textoCifradoElement.placeholder = 'Tu clave';
+
+            usuarioPublicoElement.disabled = false;
+            usuarioPublicoElement.placeholder = 'Usuario';
+        }, 3000);
+
+        // Mostrar error
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'No se pudo crear el usuario. ' + error.message
+        });
+
+    } finally {
+        toggleForm(thisRegisterForm, false);
+    }
+});
+
+// Login de usuario
+const thisLoginForm = document.getElementById('thisLoginForm');
+thisLoginForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const textoCorreoLogin = document.getElementById('textoCorreoLogin').value.toLowerCase();
+    const textoCifradoLogin = document.getElementById('textoCifradoLogin').value;
+
+    try {
+        // Iniciar sesión con correo y contraseña
+        const userCredential = await signInWithEmailAndPassword(auth, textoCorreoLogin, textoCifradoLogin);
+        const user = userCredential.user;
+
+        // Obtener el nombre de usuario desde Firestore
+        const userDoc = await getDoc(doc(db, 'users', user.uid));
+        if (userDoc.exists()) {
+            const userData = userDoc.data();
+            Swal.fire({
+                icon: 'success',
+                html: `<span>Bienvenido de nuevo! <strong>${userData.username}</strong></span>`,
+                timer: 3000,
+                timerProgressBar: true,
+                showConfirmButton: false,
+                didClose: () => {
+                    window.location.href = '../../public/';
+                }
+            });
+        } else {
+            Swal.fire({
+                icon: 'error',
+                toast: true,
+                timer: 3000,
+                timerProgressBar: true,
+                html: 'Usuario no encontrado.'
+            });
+        }
+    } catch (error) {
+        // RESETEAR VALORES (ANTISPAM)
+        const textoCorreoLogin = document.getElementById('textoCorreoLogin');
+        const textoCifradoLogin = document.getElementById('textoCifradoLogin');
+
+        textoCorreoLogin.value = '';
+        textoCifradoLogin.value = '';
+
+        function preventSX() {
+            textoCorreoLogin.placeholder = 'Espere 3 segundos..';
+            textoCorreoLogin.disabled = true;
+
+            textoCifradoLogin.disabled = true;
+            textoCifradoLogin.placeholder = 'Espere 3 segundos';
+        }
+        preventSX();
+
+        setTimeout(() => {
+            textoCorreoLogin.placeholder = 'Ingresa tu correo';
+            textoCorreoLogin.disabled = false;
+
+            textoCifradoLogin.disabled = false;
+            textoCifradoLogin.placeholder = 'Tu clave';
+        }, 3000);
+
+        // Mostrar error
+        Swal.fire({
+            icon: 'error',
+            toast: true,
+            timer: 3000,
+            position: 'top-end',
+            html: `${error.message}`,
+            showConfirmButton: false
+        });
+    }
+});
+
+// DECLARANDO TODAS LAS VARIABLES Y FUNCIONES GENERALES
+const thisULogin = document.getElementById('thisULogin');
+
+// FUNCIONES PRINCIPALES
+thisULogin.addEventListener('click', () => {
+    Swal.fire({
+        imageUrl: '../assets/images/secondPolice_OfficerPixel_Art.gif',
+        html: `
+        <ul style='list-style:none;'>
+            <li>Evita el spam :)</li> 
+            <li>Utiliza nombres cortos</li> 
+            <li>Evita la información personal</li>
+            <strong>Evita utilizar correos personales</strong>
+        </ul>
+        `,
+        timer: 6000,
+        timerProgressBar: true
+    });
+});
+
+// FUNCION CARGA DE PAGINA
+window.onload = function () {
+    Swal.fire({
+        imageUrl: '../assets/images/firstScript_TablePixelArt.gif',
+        html: `<li style='list-style:none;'>Por favor lee las reglas :)</li>`,
+        timer: 5000,
+        timerProgressBar: true,
+        showConfirmButton: false,
+        showCancelButton: false
+    });
+}
